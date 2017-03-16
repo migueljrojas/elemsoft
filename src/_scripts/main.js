d483ccaabe688ed80fb74b4d19b7f8c34f68d09c
migueljrojas <migueljrojas@gmail.com>
2017-03-16 18:32:39 -0300

    Inner structure

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -7,12 +7,14 @@ var $ = require('jquery');
 // var bootstrap = require('../../bower_components/bootstrap-sass/assets/javascripts/Bootstrap');
 var header = require('../_modules/header/header');
 var home = require('./pages/home.js');
+var home = require('./pages/servicios.js');
 var clientShowcase = require('../_modules/client-showcase/clientShowcase');
 
 $(function() {
     require('../../bower_components/slick-carousel/slick/slick');
     new header();
     new home();
+    new servicios();
     new clientShowcase();
     console.log('Welcome to Yeogurt!');
 });
---
+++ src/_scripts/pages/servicios.js
@@ -0,0 +1,8 @@
+'use strict';
+var $ = require('jquery');
+
+var Servicios = function() {
+
+};
+
+module.exports = Servicios;
